e427530136daf1421c2be3ee0cbd4a2dedb56529
AlexandrKoliukh <alexkoliukh@gmail.com>
2019-04-09 20:56:47 +0300

    step4 remarks fix

--- src/bin/brain-even.js
+++ src/bin/brain-even.js
@@ -1,9 +1,5 @@
 #!/usr/bin/env node
 
-import { greeting, createQuestion } from '..';
+import game from '..';
 
-console.log('Welcome to the Brain Games!');
-console.log('Answer "yes" if number even otherwise answer "no".\n');
-
-greeting();
-createQuestion(3);
+game();
--- src/index.js
+++ src/index.js
@@ -1,34 +1,35 @@
 import readlineSync from 'readline-sync';
 
-const getUserName = () => readlineSync.question('May I have your name? ');
-const getUserAnswer = () => readlineSync.question('Your answer: ');
+export default () => {
+  console.log('Welcome to the Brain Games!');
+  console.log('Answer "yes" if number even otherwise answer "no".\n');
 
-let userName;
-
-export const greeting = () => {
-  userName = getUserName();
+  const userName = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${userName}!`);
-};
 
-export const createQuestion = (questionCount) => {
-  const correctAnswer = 'Correct!';
-  const checkCorrectAnswer = (userAnswer, rightAnswer) => `'${userAnswer}' is wrong answer ;(. Correct answer was '${rightAnswer}'. Let's try again, ${userName}!`;
+  const isEven = arg => arg % 2 === 0;
 
-  for (let i = 0; i < questionCount; i += 1) {
-    const questionValue = Math.floor(Math.random() * 100);
-    console.log(`Question: ${questionValue}`);
+  const createQuestion = (questionCount) => {
+    const correctAnswer = 'Correct!';
+    const checkCorrectAnswer = (userAnswer, rightAnswer) => `'${userAnswer}' is wrong answer ;(. Correct answer was '${rightAnswer}'. Let's try again, ${userName}!`;
 
-    const userAnswer = getUserAnswer();
+    for (let i = 0; i < questionCount; i += 1) {
+      const question = Math.floor(Math.random() * 100);
+      console.log(`Question: ${question}`);
 
-    const varAnswer1 = questionValue % 2 === 0 && userAnswer === 'yes';
-    const varAnswer2 = questionValue % 2 !== 0 && userAnswer === 'no';
+      const userAnswer = readlineSync.question('Your answer: ');
+      const varAnswer1 = isEven(question) && userAnswer === 'yes';
+      const varAnswer2 = !isEven(question) && userAnswer === 'no';
 
-    if (varAnswer1 || varAnswer2) console.log(correctAnswer);
-    else {
-      const rightAnswer = questionValue % 2 === 0 ? 'yes' : 'no';
-      console.log(checkCorrectAnswer(userAnswer, rightAnswer));
-      return;
+      if (varAnswer1 || varAnswer2) console.log(correctAnswer);
+      else {
+        const rightAnswer = isEven(question) ? 'yes' : 'no';
+        console.log(checkCorrectAnswer(userAnswer, rightAnswer));
+        return;
+      }
     }
-  }
-  console.log(`Congratulations, ${userName}!`);
+    console.log(`Congratulations, ${userName}!`);
+  };
+
+  createQuestion(3);
 };
