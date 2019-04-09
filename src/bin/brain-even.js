a2c5cdf2d364bccf86f36f2c6f9ae565b81085d5
AlexandrKoliukh <alexkoliukh@gmail.com>
2019-04-09 15:03:08 +0300

    step4-1

--- src/bin/brain-even.js
+++ src/bin/brain-even.js
@@ -5,5 +5,6 @@ import { greeting, createQuestion } from '..';
 console.log('Welcome to the Brain Games!\n');
 
 greeting();
+
 console.log('\nAnswer "yes" if number even otherwise answer "no".');
 createQuestion(3);
