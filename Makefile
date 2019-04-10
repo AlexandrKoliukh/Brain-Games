4840bccf789707e50bf87792240a1ae89b10d6ce
AlexandrKoliukh <alexkoliukh@gmail.com>
2019-04-10 18:01:18 +0300

    add progression game

--- Makefile
+++ Makefile
@@ -3,7 +3,7 @@ install:
 
 
 start:
-	npx babel-node src/bin/brain-gcd.js
+	npx babel-node src/bin/brain-progression.js
 
 publish:
 	npm publish
---
+++ src/games/progression.js
@@ -0,0 +1,35 @@
+import makeGame from '..';
+import getRndNumber from '../utils';
+import { cons } from 'hexlet-pairs';
+
+const progressionLength = 10;
+const gameDescription = 'What number is missing in the progression?';
+
+const getProgression = () => {
+  const progressionArray = new Array(progressionLength);
+  const startProgressionValue = getRndNumber();
+  const progressionWeight = getRndNumber();
+  for (let i = 0; i < progressionLength; i += 1) {
+    if (i === 0) progressionArray[i] = startProgressionValue;
+    else progressionArray[i] = progressionArray[i - 1] + progressionWeight;
+  }
+  return progressionArray;
+};
+
+const hideElement = (progression, element) => progression.map((i) => {
+  if (progression.indexOf(i) === element) return '..';
+  return i;
+});
+
+const getGameProgressionData = () => {
+  const progression = getProgression();
+  const indexOfHiddenElement = getRndNumber(progressionLength);
+  const progressionWithHiddenElement = hideElement(progression, indexOfHiddenElement);
+  const question = progressionWithHiddenElement.reduce((i, acc) => `${i} ${acc}`, '');
+  const rightAnswer = progression[indexOfHiddenElement];
+  return cons(question, rightAnswer);
+};
+
+export default () => {
+  makeGame(gameDescription, getGameProgressionData);
+};
