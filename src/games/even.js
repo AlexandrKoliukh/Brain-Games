e27a8e712db8700ef4cb75b27dd7ec69c7bc7cd3
AlexandrKoliukh <alexkoliukh@gmail.com>
2019-04-10 18:03:06 +0300

    style

--- src/games/even.js
+++ src/games/even.js
@@ -6,14 +6,9 @@ const gameDescription = 'Answer "yes" if number even otherwise answer "no".';
 
 const isEven = arg => arg % 2 === 0;
 
-const getRightAnswer = (question) => {
-  if (isEven(question)) return 'yes';
-  return 'no';
-};
-
 const getGameEvenData = () => {
   const question = getRndNumber();
-  const rightAnswer = getRightAnswer(question);
+  const rightAnswer = isEven(question) ? 'yes' : 'no';
   return cons(question, rightAnswer);
 };
 
